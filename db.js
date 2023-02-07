8b1ad17de7746d9c95ed4157923e5678d40fcbbb
1996Bella <chiang63158@gmail.com>
2023-02-07 22:14:25 +0800

    第一次上傳

---
+++ db.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+function apple(sql, data, callback) {
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'chingshin',
+        multipleStatements: true, 
+    });
+    connection.connect();
+
+    connection.query(sql, data, function (error, results, fields) {
+        if (error) {
+            console.log(error)
+        };
+        callback(results, fields);
+    })
+    connection.end();
+}
+
+module.exports.exec = apple;
